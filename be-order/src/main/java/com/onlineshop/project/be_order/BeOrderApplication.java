1a61e4ed20e756ffa72d0fd498afbd14615b2cfd
MuhammadRasyidF <rasyid201101@gmail.com>
2024-08-07 11:34:03 +0700

    init

---
+++ be-order/src/main/java/com/onlineshop/project/be_order/BeOrderApplication.java
@@ -0,0 +1,13 @@
+package com.onlineshop.project.be_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeOrderApplication.class, args);
+	}
+
+}

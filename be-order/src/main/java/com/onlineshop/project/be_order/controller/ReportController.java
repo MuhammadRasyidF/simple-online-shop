ed70d7ea0cb595a0b5020b764133a76782c6f8c1
MuhammadRasyidF <rasyid201101@gmail.com>
2024-09-19 15:24:06 +0700

    adding jasper report

--- be-order/src/main/java/com/onlineshop/project/be_order/controller/ReportController.java
+++ be-order/src/main/java/com/onlineshop/project/be_order/controller/ReportController.java
@@ -1,4 +1,49 @@
 package com.onlineshop.project.be_order.controller;
 
+import com.onlineshop.project.be_order.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api/order")
+@CrossOrigin(origins = "*")
 public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
+
+    @GetMapping("/report")
+    public ResponseEntity<?> downloadOrderReport() {
+        try {
+            byte[] pdfReport = reportService.generateOrderReport();
+            LocalDate today = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            String formattedDate = today.format(formatter);
+
+            String outputFilename = "order_report_" + formattedDate + ".pdf";
+            ByteArrayResource resource = new ByteArrayResource(pdfReport);
+            return ResponseEntity.ok()
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .contentLength(resource.contentLength())
+                    .header(HttpHeaders.CONTENT_DISPOSITION,
+                            ContentDisposition.attachment().filename(outputFilename).build().toString())
+                    .body(resource);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("gagal membuat report".getBytes());
+        }
+    }
 }
--- be-order/src/main/java/com/onlineshop/project/be_order/service/ReportService.java
+++ be-order/src/main/java/com/onlineshop/project/be_order/service/ReportService.java
@@ -1,4 +1,34 @@
 package com.onlineshop.project.be_order.service;
 
+import com.onlineshop.project.be_order.model.Order;
+import com.onlineshop.project.be_order.repository.OrderRepository;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class ReportService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public byte[] generateOrderReport() throws Exception{
+        File file = ResourceUtils.getFile("be-order/src/main/resources/order_report.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        List<Order> orders = orderRepository.findAll();
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(orders);
+        Map<String, Object> parameters = new HashMap<>();
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+
+        return JasperExportManager.exportReportToPdf(jasperPrint);
+    }
+
 }
